062e472062fc7363c0205dd9941631453405fdda
shalini <shalini.v@tvsnext.io>
2021-07-03 16:15:59 +0530

    [shalini]Editing Contact

---
+++ AddressBook/AddBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddBook
+    {
+        static void Main(string[] args)
+        {
+            AddressBook.AddrBook.GetCustomer();
+            AddressBook.AddrBook.Modify();
+            AddressBook.AddrBook.ListingPeople();
+            // Sample.AddrBook.RemovePeople();
+
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,24 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
+namespace Sample
 {
-    class AddressesBook
+    class AddrBook
     {
+
         public static List<Person> People = new List<Person>();
         public class Person
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
-            public string Addressess { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Addresses { get; set; }
             public string City { get; set; }
             public string State { get; set; }
-            public string PhoneNumber { get; set; }
             public string ZipCode { get; set; }
-            public string Email { get; set; }
-
+            public string PhoneNum { get; set; }
+            public string EmailId { get; set; }
         }
-        private static void GetPerson()
+        public static void GetCustomer()
         {
             Person person = new Person();
 
@@ -32,7 +33,7 @@ namespace AddressBook
             person.LastName = Console.ReadLine();
 
             Console.Write("Enter Address : ");
-            person.Addressess = Console.ReadLine();
+            person.Addresses = Console.ReadLine();
 
             Console.Write("Enter City : ");
             person.City = Console.ReadLine();
@@ -42,8 +43,13 @@ namespace AddressBook
 
             Console.Write("Enter ZipCode: ");
             person.ZipCode = Console.ReadLine();
-            Console.Write("Enter ZipCode: ");
-            person.Email = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNum = Console.ReadLine();
+
+            Console.Write("Enter EmailId: ");
+            person.EmailId = Console.ReadLine();
+
             People.Add(person);
         }
         public static void PrintCustomer(Person person)
@@ -51,14 +57,85 @@ namespace AddressBook
             Console.WriteLine("First Name: " + person.FirstName);
             Console.WriteLine("Last Name: " + person.LastName);
             Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Address : " + person.Addressess);
+            Console.WriteLine("Address : " + person.Addresses);
             Console.WriteLine("City : " + person.City);
             Console.WriteLine("State : " + person.State);
             Console.WriteLine("ZipCode : " + person.ZipCode);
-            Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("EmailId: " + person.Email);
+            Console.WriteLine("Phone Number: " + person.PhoneNum);
+            Console.WriteLine("Email Id: " + person.EmailId);
             Console.WriteLine("-------------------------------------------");
         }
+        public static void Modify()
+        {
+            if (People.Count != 0)
+            {
+                Console.WriteLine("Enter the contact to modify:");
+                string Modified = Console.ReadLine();
+                foreach (var person in People)
+                {
+                    if (person.FirstName.ToUpper() == Modified.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the option to modify the property: ");
+                            Console.WriteLine("Enter 1 to Change First name ");
+                            Console.WriteLine("Enter 2 to Change Last name ");
+                            Console.WriteLine("Enter 3 to Change Phone Number ");
+                            Console.WriteLine("Enter 4 to Change Address ");
+                            Console.WriteLine("Enter 5 to Change City ");
+                            Console.WriteLine("Enter 6 to Change State ");
+                            Console.WriteLine("Enter 7 to Change Pincode ");
+                            Console.WriteLine("Enter 8 to Exit ");
+                            int Check = Convert.ToInt32(Console.ReadLine());
+                            switch (Check)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.FirstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.LastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.PhoneNum = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.Addresses = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.City = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.State = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.ZipCode = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    return;
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter the valid name!");
+                    }
+
+                }
+
+
+            }
+        }
+
         public static void ListingPeople()
         {
             if (People.Count == 0)
@@ -76,13 +153,5 @@ namespace AddressBook
             Console.ReadKey();
         }
 
-
-        static void Main(string[] args)
-        {
-            AddressBook.AddressesBook.GetPerson();
-            AddressBook.AddressesBook.ListingPeople();
-
-
-        }
     }
 }
